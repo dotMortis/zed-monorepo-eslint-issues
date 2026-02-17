d411279128adac14791d89962142b514804d096f
Nick K. <nick.koellhofer@braun-its.de>
2026-02-17 19:03:26 +0100

    add floating await to validate eslint rules

--- packages/a/src/a.ts
+++ packages/a/src/a.ts
@@ -1,6 +1,7 @@
-export function DebugA(): void {
+export function DebugA(): Promise<void> {
         // should have 8 tab width
         // should warn -> Unexpected console statement. (eslint no-console)
         // should have single quotes
         console.log('hello from a');
+        return Promise.resolve();
 }
